40e4585c8d0cfa9b829ba26e7b0c09a348c52045
lucascs20182 <lucascs20182@gmail.com>
2021-01-21 17:14:12 -0300

    implemented the component events

--- reactjs-mac-like-calculator/src/components/Button.jsx
+++ reactjs-mac-like-calculator/src/components/Button.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import './Button.css'
 
-const buttonComponent = props =>
-    <button className='button'>{props.label}</button>
-
-
+const buttonComponent = props => {
+    let classes = 'button'
+    classes += props.operation ? ' operation' : ''
+    classes += props.double ? ' double' : ''
+    classes += props.triple ? ' triple' : ''
+    
+    return (
+        <button onClick={e => props.click && props.click(props.label)}
+            className={classes}>
+                {props.label}
+        </button>
+    )
+}
+    
 export default buttonComponent
